44dfc7d906625cf0f74e5f018700552f9154241e
vlad4400 <sleepvs@gmail.com>
2024-12-03 17:03:29 +0100

    added process simulation

--- src/app/components/strategy-selector/strategy-selector.component.ts
+++ src/app/components/strategy-selector/strategy-selector.component.ts
@@ -4,11 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { timer } from 'rxjs';
+import { DialogModule } from 'primeng/dialog';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-strategy-selector',
   standalone: true,
-  imports: [FormsModule, DropdownModule, InputTextModule, ButtonModule],
+  imports: [FormsModule, DropdownModule, InputTextModule, ButtonModule, DialogModule, ProgressSpinnerModule],
   templateUrl: './strategy-selector.component.html',
   styleUrls: ['./strategy-selector.component.scss'] 
 })
@@ -19,10 +22,44 @@ export class StrategySelectorComponent {
   amount: number = 1000;
   frequency: string = 'Miesięczna';
   simulationResults: SimulationResult[] = [];
+
+  showProgressDialog: boolean = false;
+  progressMessage: string = '';
+
   @Output() simulationResult = new EventEmitter<SimulationResult>(); 
 
+  
+
   constructor(private creditService: CreditSimulationService) {}
- simulate() {
+  simulate() {
+    this.showProgressDialog = true;
+    this.progressMessage = 'Przygotowywanie danych...';
+
+    const processSteps = [
+      { message: 'Przygotowywanie danych...', duration: 500 },
+      { message: 'Symulacja strategii...', duration: 1000 },
+      { message: 'Przetwarzanie wyników...', duration: 1500 },
+      { message: 'Zakończono!', duration: 500 },
+    ];
+
+    let totalDuration = 0;
+
+    processSteps.forEach((step, index) => {
+      totalDuration += step.duration;
+      timer(totalDuration).subscribe(() => {
+        this.progressMessage = step.message;
+
+        if (index === processSteps.length - 1) {
+          setTimeout(() => {
+            this.performSimulation();
+            this.showProgressDialog = false;
+          }, step.duration);
+        }
+      });
+    });
+  }
+
+  performSimulation() {
     const result = this.creditService.calculateStrategy(
       this.amount,
       this.frequency,
@@ -32,6 +69,4 @@ export class StrategySelectorComponent {
     );
     this.simulationResult.emit(result);
   }
-
-  
 }
--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, InputTextModule, ButtonModule],
+  imports: [FormsModule, InputTextModule, ButtonModule, DialogModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -15,11 +17,35 @@ export class LoginComponent {
 
   @Output() loggedIn = new EventEmitter<boolean>();
 
+  showProgressDialog: boolean = false;
+  progressMessage: string = '';
+
   onSubmit() {
-    if (this.username === 'demo' && this.password === 'Qwerty123') {
-      this.loggedIn.emit(true);
-    } else {
-      alert('Nieprawidłowy login lub hasło');
-    }
+    this.showProgressDialog = true;
+    this.progressMessage = 'Trwa wysyłanie danych...';
+
+    const processSteps = [
+      { message: 'Trwa wysyłanie danych...', duration: 500 },
+      { message: 'Sprawdzanie loginu i hasła...', duration: 1500 },
+      { message: 'Nadawanie dostępu dla użytkownika...', duration: 1000 },
+      { message: 'Sukces!', duration: 500 },
+    ];
+
+    let totalDuration = 0;
+
+    processSteps.forEach((step, index) => {
+      totalDuration += step.duration;
+      timer(totalDuration).subscribe(() => {
+        this.progressMessage = step.message;
+
+        if (index === processSteps.length - 1) {
+          setTimeout(() => {
+            this.showProgressDialog = false;
+            this.loggedIn.emit(true);
+          }, step.duration);
+        }
+      });
+    });
   }
 }
+
