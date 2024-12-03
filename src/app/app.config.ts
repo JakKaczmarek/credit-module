a2abcda4ec96d434406f44772049c60ce76b1b7e
vlad4400 <sleepvs@gmail.com>
2024-12-03 17:19:52 +0100

    fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeFrCH from '@angular/common/locales/fr-CH';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
+registerLocaleData(localeFrCH, 'fr-CH');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations(), // Używane do animacji
+    provideAnimations(),
+    { provide: LOCALE_ID, useValue: 'fr-CH' },
   ]
 };
