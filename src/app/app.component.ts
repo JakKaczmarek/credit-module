bba9733b155bebf2275d07ba98558308e808f264
vlad4400 <sleepvs@gmail.com>
2024-12-03 16:13:42 +0100

    styled UI compoenents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,19 @@ import { TableModule } from 'primeng/table';
 import { StrategySelectorComponent } from './components/strategy-selector/strategy-selector.component';
 import { SimulationResultComponent } from './components/simulation-result/simulation-result.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [StrategySelectorComponent, SimulationResultComponent, FormsModule, DropdownModule, InputTextModule, ButtonModule, TableModule, CommonModule
-  ],
+  imports: [StrategySelectorComponent, SimulationResultComponent, FormsModule, DropdownModule, InputTextModule, ButtonModule, TableModule, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'credit-module';
-  simulationResults : any = []; 
+  simulationResults: any = [];
 
   handleSimulationResult(result: any) {
     this.simulationResults.push(result);
